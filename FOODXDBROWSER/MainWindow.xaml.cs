2e404fd5e49581324334274fb6ddd15b11acc8ed
Frogithezhrda <omersaban169@gmail.com>
2025-02-17 19:24:47 +0200

    Upgraded Browser Look Fixed A Lot Of Logic Made All Buttons Work, Made Button Hover

--- FOODXDBROWSER/MainWindow.xaml.cs
+++ FOODXDBROWSER/MainWindow.xaml.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using CefSharp.Wpf;
+using CefSharp;
 
 namespace FOODXDBROWSER
 {
@@ -21,8 +13,8 @@ namespace FOODXDBROWSER
     /// </summary>
     public partial class MainWindow : Window
     {
-        LinkedList<string> WebPagesFront;
-        LinkedList<string> WebPagesBack;
+        LinkedList<string> WebPagesFront = new LinkedList<string>();
+        LinkedList<string> WebPagesBack = new LinkedList<string>();
         bool _addToList = true;
         public MainWindow()
         {
@@ -35,34 +27,42 @@ namespace FOODXDBROWSER
             Chrome.Address = Link;
             if (addToList)
             {
-                WebPagesBack.AddFirst(Link);
+                if (WebPagesBack.Count == 0 || WebPagesBack.First() != Link)
+                {
+                    WebPagesBack.AddFirst(Link);
+                }
             }
         }
         void GoHome()
         {
-            Search.Text = "google.com";
-            Chrome.Address = "google.com";
+            WebPagesFront.Clear();
+            WebPagesBack.Clear();
+            LoadWebPages("https://www.google.com");
         }
         void ToggleWebPages(string Option)
         {
-            if(Option == "←")
+            if(Option == "←" && WebPagesBack.Count > 1)
             {
-                if((WebPagesBack.Count) > 1)
-                {
+
                     WebPagesFront.AddFirst(WebPagesBack.First());
                     WebPagesBack.RemoveFirst();
-                    LoadWebPages(WebPagesBack.First(), false);
-                }
+                    LoadWebPages(WebPagesBack.First());
             }
-            else
+            else if (WebPagesFront.Count != 0)
             {
-                if((WebPagesFront.Count != 0))
-                {
-                    LoadWebPages(WebPagesFront.First(), false);
-                    WebPagesBack.AddFirst(WebPagesFront.First());
-                    WebPagesFront.RemoveFirst();
-                }
+
+                string nextPage = WebPagesFront.First();
+                WebPagesFront.RemoveFirst();
+                LoadWebPages(nextPage);
             }
+        }
+        private void UpdateNavigationButtons()
+        {
+            Back.FontWeight = (WebPagesBack.Count > 1) ? FontWeights.Bold : FontWeights.Normal;
+            Back.Foreground = (WebPagesBack.Count > 1) ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.DarkSlateGray);
+
+            Front.FontWeight = (WebPagesFront.Count > 0) ? FontWeights.Bold : FontWeights.Normal;
+            Front.Foreground = (WebPagesFront.Count > 0) ? new SolidColorBrush(Colors.Black) : new SolidColorBrush(Colors.DarkSlateGray);
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -71,44 +71,45 @@ namespace FOODXDBROWSER
         }
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            LoadWebPages(Search.Text, false);
+            Chrome.Reload();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
+            GoHome();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            WebPagesBack = new LinkedList<string>();
-            WebPagesFront = new LinkedList<string>();
-            GoHome();
             Chrome.AddressChanged += Chrome_AddressChanged;
-            LoadWebPages(Search.Text, false);
-            _addToList = true;
+            GoHome();
         }
 
 
         private void Chrome_AddressChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
+            //invoke when the address is changed
             Dispatcher.Invoke(() =>
             {
+                //getting the value to thee text
                 Search.Text = e.NewValue.ToString();
-                if(_addToList)
+                if (_addToList && (WebPagesBack.Count == 0 || WebPagesBack.First() != Search.Text))
                 {
                     WebPagesBack.AddFirst(Search.Text);
+                    WebPagesFront.Clear();
                 }
+                UpdateNavigationButtons();
             });
         }
         private void Search_KeyDown(object sender, KeyEventArgs e)
         {
+            //when key enter is down
             if(e.Key == Key.Enter)
             {
-                LoadWebPages(Search.Text, false);
-                _addToList = true;
+                //load the page and add it
+                LoadWebPages(Search.Text, true);
             }
         }
-
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
         }
@@ -117,35 +118,7 @@ namespace FOODXDBROWSER
             e.Handled = true;   
         }
 
-        private void Browser_Click(object sender, RoutedEventArgs e)
-        {
 
-            if(Search.Text == "google.com")
-            {
-                Search.Text = "search.brave.com";
-                LoadWebPages(Search.Text);
-            }
-            else
-            {
-                Search.Text = "google.com";
-                LoadWebPages(Search.Text);
-            }
-        }
-
-        private void History_Click(object sender, RoutedEventArgs e)
-        {
-        }
-
-        private void HistoryPart_Click(object sender, RoutedEventArgs e)
-        {
-            LoadWebPages(Chrome.Address);
-        }
-
-       
-
-        private void DarkMode_Click(object sender, RoutedEventArgs e)
-        {
-        }
 
         private void Search_TextChanged(object sender, TextChangedEventArgs e)
         {
